acc9cf4b28efab9d7c6c8a2a998ea1db7f15f4b1
fantasy2023 <fantasy201016@gmail.com>
2020-03-29 01:21:59 +0300

    demo added

--- src/App.js
+++ src/App.js
@@ -5,22 +5,91 @@ import Skeleton from './components/Skeleton';
 function App() {
 
   const [isLoaded, setLoaded] = useState(false)
+  const [isLoadedDemo, setLoadedDemo] = useState(false)
 
   return (
     <div className="container">
-      <Skeleton
-        width = "200px"
-        height = "40px"
-        loaded = {isLoaded}
-      >
-        <a>tests</a>
-      </Skeleton>
+      <div style={{marginTop:"10px", zIndex:"3"}}>
+        <h3>Separated Components</h3>
+        <Skeleton width="200px" height="30px" loaded={false}>
+          <a>tests</a>
+        </Skeleton>
+
+        <Skeleton width="60px" height="60px" loaded={false} shape="circle">
+          <img src="https://picsum.photos/60/60" />
+        </Skeleton>
+
+        <Skeleton width="200px" height="30px" loaded={isLoaded}>
+          <a>tests</a>
+        </Skeleton>
+
+        <Skeleton width="60px" height="60px" loaded={isLoaded} shape="circle" duration={8000}>
+          <img
+            src="https://picsum.photos/id/231/60/60"
+            style={{ borderRadius: "50%" }}
+          />
+        </Skeleton>
+        <Skeleton width="60px" height="60px" loaded={isLoaded} shape="circle">
+          <img
+            src="https://picsum.photos/id/231/60/60"
+            style={{ borderRadius: "50%" }}
+          />
+        </Skeleton>
 
 
-      <div style={{display: "inherit", marginTop: "50px"}}>
-        <button onClick = {() => setLoaded(!isLoaded)}>Push data</button>
+        <div style={{display: "inherit", marginTop: "50px"}}>
+          <button onClick = {() => setLoaded(!isLoaded)}>Push data</button>
+        </div>
       </div>
 
+      <hr style={{width:"100%"}}/>
+      {
+      // <div className="linkedin-card">
+      //   <Skeleton width="90px" height="90px" loaded={isLoaded} shape="circle" style={{top:"150px",left:"85px", zIndex:"2"}} backgroundColor="#e1e9ee">
+      //       <img
+      //         src="https://picsum.photos/id/231/90/90"
+      //         style={{ borderRadius: "50%",top:"150px",left:"85px", zIndex:"2", display:"inline-block" }}
+      //       />
+      //   </Skeleton>
+      //   <Skeleton width="260px" height="100px" loaded={isLoaded} style={{borderTopRightRadius: "5px", borderTopLeftRadius: "5px", display:"flex"}} backgroundColor="#D3DCE2">
+      //     <img src="https://picsum.photos/260/100" />
+      //   </Skeleton>
+      //   <Skeleton width="260px" height="290px" loaded={isLoaded} style={{borderBottomRightRadius: "5px", borderBottomLeftRadius: "5px"}}>
+      //     <Skeleton width="200px" height="20px" loaded={isLoaded} backgroundColor="#E7EDF1" style={{marginTop:"60px"}}>
+      //       <a>Anıl Şenay</a>
+      //     </Skeleton>
+      //     <Skeleton width="150px" height="20px" loaded={isLoaded} backgroundColor="#E7EDF1" style={{marginTop:"10px"}}>
+      //       <a>Marmara University</a>
+      //     </Skeleton>
+      //   </Skeleton>
+      // </div>
+      }
+      
+      <div className="linkedin-card">
+        <Skeleton width="90px" height="90px" loaded={isLoadedDemo} shape="circle" style={{top:"150px",left:"85px", zIndex:"2"}} backgroundColor="#e1e9ee">
+          <img src="https://picsum.photos/id/231/90/90"
+            style={{ borderRadius: "50%",top:"150px",left:"85px", zIndex:"2", display:"inline-block", position:"relative" }}
+          />
+        </Skeleton>
+
+        <Skeleton width="260px" height="100px" loaded={isLoadedDemo} style={{borderTopRightRadius: "5px", borderTopLeftRadius: "5px", display:"flex"}} backgroundColor="#D3DCE2">
+            <div style={{width: "260px", height: "100px", backgroundColor:"white"}}>
+              <img src="https://picsum.photos/260/100" />
+            </div>
+        </Skeleton>
+        
+        <div style={{width: "260px", height: "290px", backgroundColor:"white", textAlign:"center", display: "inline-block"}}>
+          <Skeleton width="200px" height="20px" loaded={isLoadedDemo} backgroundColor="#E7EDF1" style={{marginTop:"60px"}}>
+            <a style={{marginTop:"60px", display:"block", fontWeight:"bold"}}>Anıl Şenay</a>
+          </Skeleton>
+          <Skeleton width="150px" height="20px" loaded={isLoadedDemo} backgroundColor="#E7EDF1" style={{marginTop:"10px"}}>
+            <a style={{marginTop:"10px", display:"block"}}>Marmara University</a>
+          </Skeleton>
+        </div>
+      </div>
+      <div style={{display: "inherit", marginTop: "50px", position:"relative", top:"-120px"}}>
+          <button onClick = {() => setLoadedDemo(!isLoadedDemo)}>Push data</button>
+        </div>
     </div>
   );
 }
--- src/components/Skeleton.js
+++ src/components/Skeleton.js
@@ -1,20 +1,41 @@
-import React, { useState } from 'react'
+import React, {useState} from 'react'
 
 export default function Skeleton(props) {
 
+    const [isHidden, setHidden] = useState(false)
+
+    const hideSkeleton = () => {
+        setHidden(true)
+    }
+
     let skeletonStyle = {}
-    
+
     props.width !== null ? skeletonStyle.width = props.width : skeletonStyle.width = ""
     props.height !== null ? skeletonStyle.height = props.height : skeletonStyle.height = ""
+    props.shape === "circle" ? (skeletonStyle.borderRadius = "50%") : (skeletonStyle.borderRadius = "");
+    props.style !== null ? skeletonStyle = {...skeletonStyle, ...props.style} : void(0)
+    props.backgroundColor !== null ? skeletonStyle.backgroundColor = props.backgroundColor : skeletonStyle.backgroundColor = "white"
+    
+    if(props.duration !== null && props.duration !== undefined)
+        setTimeout(hideSkeleton, props.duration)
 
+    let skeletonChildren = []
+    if(props.children !== undefined){
+        for(let i = 0; i < props.children.length; i++){
+            if(props.children[i].type.name === "Skeleton")
+                skeletonChildren.push(props.children[i])
+        }
+    }
+    console.log(skeletonChildren)
     return (
         <div>
             {            
-            props.loaded ? props.children : 
+            props.loaded || isHidden ? props.children : 
         
-            <span className="skeleton-box" style={skeletonStyle} >
+                <div className="skeleton-box" style={skeletonStyle}>
+                    {skeletonChildren.length > 0 ? skeletonChildren : null}
+                </div>
 
-            </span>
             }
         </div>
     )
